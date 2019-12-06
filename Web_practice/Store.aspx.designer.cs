6746eb487f2d9bd9fe7a48fc505aa4512dfa0db8
dogQ <dog98726345@gmail.com>
2019-12-06 17:39:37 +0800

    class 7 finished

--- Web_practice/Store.aspx.designer.cs
+++ Web_practice/Store.aspx.designer.cs
@@ -148,6 +148,15 @@ namespace Web_practice {
         protected global::System.Web.UI.WebControls.SqlDataSource orderItemDataSource;
         
         /// <summary>
+        /// totalLabel 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label totalLabel;
+        
+        /// <summary>
         /// drinkData 控制項。
         /// </summary>
         /// <remarks>
